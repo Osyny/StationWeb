e5c1667a6d1afd759d7b6a96b0c99c3d2fe34e0d
oksanaD <oxana1404@gmail.com>
2024-11-21 16:37:47 +0200

    added spiner component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AbpValidationSummaryComponent } from './validation/abp-validation-summary.component';
 import { AbpModalHeaderComponent } from './modal/abp-modal-header.component';
+import { SpinerComponent } from '../layout/spiner/spiner.component';
 
 @NgModule({
-  declarations: [AbpValidationSummaryComponent, AbpModalHeaderComponent],
-  exports: [AbpValidationSummaryComponent, AbpModalHeaderComponent],
+  declarations: [
+    AbpValidationSummaryComponent,
+    AbpModalHeaderComponent,
+    SpinerComponent,
+  ],
+  exports: [
+    AbpValidationSummaryComponent,
+    AbpModalHeaderComponent,
+    SpinerComponent,
+  ],
   imports: [CommonModule],
 })
 export class SharedModule {}
