129e9447cdb2ea03530345df0ae034cd8723c414
oksanaD <oxana1404@gmail.com>
2024-12-23 15:51:21 +0200

    added conector status ui

--- src/app/admin/dashboard/dtos/charge-station-displayed.dto.ts
+++ src/app/admin/dashboard/dtos/charge-station-displayed.dto.ts
@@ -1,7 +1,9 @@
 import {
   ChargeStationDto,
+  ConnectorDto,
   OwnerDto,
 } from '../../../models/charge-station.model';
+import { SelectItem } from './owner-select-list.dto';
 
 export class ChargeStationDisplayedDto implements ChargeStationDto {
   serialNumber?: string | undefined;
@@ -9,6 +11,8 @@ export class ChargeStationDisplayedDto implements ChargeStationDto {
   ownerName?: string | undefined = '';
   status: boolean = false;
   statusDisplayed: string = '';
+
   id: number = 0;
   name?: string;
+  connectors: ConnectorDto[] = [];
 }
